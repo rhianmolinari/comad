0d2ecb20dced73eefb7fe8a142412d5c453d7ac9
Rhian Molinari <molinari.rhian@gmail.com>
2021-12-14 23:38:18 -0300

    Delete all

--- wp-content/themes/hueman/parts/post-nav.php
+++
@@ -1,6 +0,0 @@
-<?php if ( is_single() ): ?>
-	<ul class="post-nav group">
-		<li class="next"><strong><?php _e('Next story', 'hueman'); ?>&nbsp;</strong><?php next_post_link('%link', '<i class="fas fa-chevron-right"></i><span>%title</span>'); ?></li>
-		<li class="previous"><strong><?php _e('Previous story', 'hueman'); ?>&nbsp;</strong><?php previous_post_link('%link', '<i class="fas fa-chevron-left"></i><span>%title</span>'); ?></li>
-	</ul>
-<?php endif; ?>
